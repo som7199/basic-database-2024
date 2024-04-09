f46e9b0acb966e5f4accb10cb9bc3a578edc2d72
som7199 <somin71991@gmail.com>
2024-04-09 15:49:53 +0900

    코테 끝

--- day09/김소민.sql
+++ day09/김소민.sql
@@ -3,7 +3,8 @@ SELECT Email
 	 , Mobile
 	 , Names
 	 , Addr
-  FROM membertbl;
+  FROM membertbl
+  ORDER BY Addr DESC, Email ASC, Names DESC;
 
 -- 2번
 SELECT Names AS '도서명'
@@ -14,42 +15,26 @@ SELECT Names AS '도서명'
   ORDER BY Price DESC;
 
 -- 3번
-SELECT *
-  FROM membertbl
- WHERE memberIdx NOT IN(SELECT memberIdx
-						 FROM rentaltbl);
-
-SELECT *
-  FROM rentaltbl
-  ORDER BY memberIdx;
-
-SELECT *
-  FROM bookstbl;
-
 SELECT m.Names AS '회원명'
 	 , m.Levels AS '회원등급'
 	 , m.Addr AS '회원주소'
 	 , r.rentalDate AS '대여일'
-	 , m.memberIdx
-  FROM bookstbl b, membertbl m, rentaltbl r
- WHERE b.bookIdx = r.bookIdx
-   AND r.memberIdx = m.memberIdx
-   AND m.memberIdx NOT IN(SELECT m.memberIdx
-							FROM bookstbl b, membertbl m, rentaltbl r
-						   WHERE b.bookIdx = r.bookIdx
-							 AND r.memberIdx = m.memberIdx);
+  FROM membertbl AS m LEFT OUTER JOIN rentaltbl AS r
+	ON r.memberIdx = m.memberIdx
+ WHERE r.rentalDate IS NULL
+   AND m.Levels = 'A';
 
--- 책 빌린 사람들
-SELECT m.memberIdx
-  FROM bookstbl b, membertbl m, rentaltbl r
- WHERE b.bookIdx = r.bookIdx
-   AND r.memberIdx = m.memberIdx
-   AND m.memberIdx NOT IN(SELECT m.memberIdx
-							FROM bookstbl b, membertbl m, rentaltbl r
-						   WHERE b.bookIdx = r.bookIdx
-							 AND r.memberIdx = m.memberIdx);
+-- 4번
+SELECT d.Names AS '책 장르'
+	 , FORMAT(SUM(b.Price), '#,#') + '원' AS '총 합계 금액'
+  FROM divtbl d, bookstbl b
+ WHERE d.Division = b.Division
+ GROUP BY d.Division, d.Names;
 
-SELECT *
-  FROM bookstbl b, membertbl m, rentaltbl r
- WHERE b.bookIdx = r.bookIdx
-   AND r.memberIdx = m.memberIdx;
+-- 5번
+SELECT ISNULL(d.Names, '--합계--') AS '책 장르'
+	 , COUNT(*) AS '권수'
+	 , FORMAT(SUM(b.Price), '#,#') + '원' AS '총 합계 금액'
+  FROM divtbl d, bookstbl b
+ WHERE d.Division = b.Division
+ GROUP BY d.Names WITH ROLLUP;
